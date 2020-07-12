94bb439c81e06d59adf3abdc028cd777f47a0bfd
1260110411 <1260110411@qq.com>
2020-07-13 00:13:45 +0800

    Thinker练习

---
+++ AutoMain.py
@@ -0,0 +1,9 @@
+#coding=UTF-8
+from tkinter import *
+
+class StartMain:
+   def Start(event):
+      # root.scr.insert(END,"SDASD")
+      s="1212"
+      print(s)
+      return s
--- AutoTest.py
+++ AutoTest.py
@@ -6,22 +6,36 @@
 from appium import webdriver
 import  time
 
-desired_caps={
+# desired_caps={
+#   "platformName": "Android",
+#   "platformVersion": "5.0",
+#   "deviceName": "GIEQM7DIZTAYY98L",
+#   "appPackage": "com.sina.weibo",
+#   "appActivity": ".SplashActivity"
+# }
+# driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_caps)
+# # 等待时间
+# time.sleep(5)
+# el1 = driver.find_element_by_id("android:id/button1")
+# el1.click()
+# # 等待时间
+# time.sleep(5)
+# el2 = driver.find_element_by_accessibility_id("我的资料")
+# el2.click()
+
+# el3 = driver.find_element_by_id("com.sina.weibo:id/btn_login")
+# el3.click()
+
+
+desired_WeiChat={
   "platformName": "Android",
   "platformVersion": "5.0",
   "deviceName": "GIEQM7DIZTAYY98L",
-  "appPackage": "com.sina.weibo",
-  "appActivity": ".SplashActivity"
+  "appPackage": "com.lenovo.coverapp.simpletime2",
+  "appActivity": "com.lenovo.coverapp.simpletime.MainActivity"
 }
-driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_caps)
+driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_WeiChat)
 # 等待时间
 time.sleep(5)
-el1 = driver.find_element_by_id("android:id/button1")
-el1.click()
-# 等待时间
-time.sleep(5)
-el2 = driver.find_element_by_accessibility_id("我的资料")
-el2.click()
-el3 = driver.find_element_by_id("com.sina.weibo:id/btn_login")
-el3.click()
+
 
